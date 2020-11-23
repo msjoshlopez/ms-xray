adc66d337de7d9e9185f217cb25ee41dead6f2b7
Tyler Bell <belltyler@live.com>
2020-11-22 19:54:09 -0500

    add prettier and format files

--- src/content.js
+++ src/content.js
@@ -1,88 +1,90 @@
 console.log("MS Xray content.js has loaded");
 
 //declare global variables
-let msFormAttributes
-let msMemberAttributes
+let msFormAttributes;
+let msMemberAttributes;
 
 function msScriptCheck() {
-    const msScriptURL = "https://api.memberstack.io/static/memberstack.js";
-    const pageScripts = document.getElementsByTagName("script");
-    let msScriptFound = false;
-    let msScripts = [];
-    let msScriptVersions = [];
-    let msIDs = [];
-    let msAttributesFound = [];
-    let msLinksFound = [];
-    let msFormsFound = [];
+  const msScriptURL = "https://api.memberstack.io/static/memberstack.js";
+  const pageScripts = document.getElementsByTagName("script");
+  let msScriptFound = false;
+  let msScripts = [];
+  let msScriptVersions = [];
+  let msIDs = [];
+  let msAttributesFound = [];
+  let msLinksFound = [];
+  let msFormsFound = [];
 
-    for (pageScript of pageScripts) {
-        if (pageScript.src.indexOf(msScriptURL) != -1) {
-            msScriptFound = true;
-            msScripts.push(pageScript.src);
-            msIDs.push(pageScript.getAttribute("data-memberstack-id"));
-            msIDs = [...new Set(msIDs)];
-        }
+  for (pageScript of pageScripts) {
+    if (pageScript.src.indexOf(msScriptURL) != -1) {
+      msScriptFound = true;
+      msScripts.push(pageScript.src);
+      msIDs.push(pageScript.getAttribute("data-memberstack-id"));
+      msIDs = [...new Set(msIDs)];
     }
+  }
 
-    if (msScripts.length > 0) {
-        for (const msScript of msScripts) {
-            // get the version of memberstack script from the array of scripts on the page
-            const split = msScript.split("?");
-            if (split.length === 1) {
-                msScriptVersions.push("No platform specified")
-            } else {
-                msScriptVersions.push(split[split.length - 1]);
-                // remove all duplicate versions from the array
-                msScriptVersions = [...new Set(msScriptVersions)];
-            }
-        }
+  if (msScripts.length > 0) {
+    for (const msScript of msScripts) {
+      // get the version of memberstack script from the array of scripts on the page
+      const split = msScript.split("?");
+      if (split.length === 1) {
+        msScriptVersions.push("No platform specified");
+      } else {
+        msScriptVersions.push(split[split.length - 1]);
+        // remove all duplicate versions from the array
+        msScriptVersions = [...new Set(msScriptVersions)];
+      }
     }
+  }
 
-    // get all the ms attributes on page and add them to msAttributesFound to show count in popup
-    msMemberAttributes = document.querySelectorAll('[data-ms-member],[data-ms-content],[data-ms-membership]');
-    for (const msMemberAttribute of msMemberAttributes) {
-        msAttributesFound.push(msMemberAttribute);
-        console.log("msAttributesFound " + msMemberAttribute);
-    };
+  // get all the ms attributes on page and add them to msAttributesFound to show count in popup
+  msMemberAttributes = document.querySelectorAll(
+    "[data-ms-member],[data-ms-content],[data-ms-membership]"
+  );
+  for (const msMemberAttribute of msMemberAttributes) {
+    msAttributesFound.push(msMemberAttribute);
+    console.log("msAttributesFound " + msMemberAttribute);
+  }
 
-    // get all the ms forms on page
-    msFormAttributes = document.querySelectorAll('[data-ms-form]');
-    for (const msFormAttribute of msFormAttributes) {
-        msFormsFound.push(msFormAttribute);
-        console.log("msFormsFound " + msFormAttribute);
-    };
+  // get all the ms forms on page
+  msFormAttributes = document.querySelectorAll("[data-ms-form]");
+  for (const msFormAttribute of msFormAttributes) {
+    msFormsFound.push(msFormAttribute);
+    console.log("msFormsFound " + msFormAttribute);
+  }
 
-    // get all the ms links on page
-    // #/ms
+  // get all the ms links on page
+  // #/ms
 
-    const msScriptMessage = {
-        msScriptFound: msScriptFound,
-        msScripts: msScripts.length,
-        msScriptVersions: msScriptVersions,
-        msIDs: msIDs,
-        msAttributesFound: msAttributesFound.length,
-        msLinksFound: msLinksFound.length,
-        msFormsFound: msFormsFound.length
-    };
-    console.log(msScriptMessage);
+  const msScriptMessage = {
+    msScriptFound: msScriptFound,
+    msScripts: msScripts.length,
+    msScriptVersions: msScriptVersions,
+    msIDs: msIDs,
+    msAttributesFound: msAttributesFound.length,
+    msLinksFound: msLinksFound.length,
+    msFormsFound: msFormsFound.length
+  };
+  console.log(msScriptMessage);
 
-    chrome.runtime.sendMessage(msScriptMessage);
+  chrome.runtime.sendMessage(msScriptMessage);
 }
 
 function msHighlightLinks() {
-    console.log("todo: highlight all links on page");
+  console.log("todo: highlight all links on page");
 }
 
 function msHighlightAttributes() {
-	for (const msAttribute of msMemberAttributes) {
-		msAttribute.style.border = "thick solid #FDFF47";
-	};
+  for (const msAttribute of msMemberAttributes) {
+    msAttribute.style.border = "thick solid #FDFF47";
+  }
 }
 
 function msHighlightForms() {
   for (const msFormAttribute of msFormAttributes) {
     msFormAttribute.style.border = "thick solid #FDFF47";
-  };
+  }
 }
 
 // messages in popup.js
@@ -90,21 +92,21 @@ function msHighlightForms() {
 // highlightAttributes
 // highlightForms
 function messageReceived(message, sender, sendResponse) {
-    console.log(message);
-    if (message.scanPage) {
-        console.log("scan the page");
-        msScriptCheck();
-    } else if (message.highlightLinks) {
-        // TODO: highlight all the elements with memberstack links on the page
-        console.log("highlight all ms links");
-        msHighlightLinks();
-    } else if (message.highlightAttributes) {
-        // TODO: highlight all the elements with memberstack data attributes
-        console.log("highlight all ms attributes");
-        msHighlightAttributes();
-    } else if (message.highlightForms) {
-        console.log("highlight all ms forms");
-        msHighlightForms();
-    }
+  console.log(message);
+  if (message.scanPage) {
+    console.log("scan the page");
+    msScriptCheck();
+  } else if (message.highlightLinks) {
+    // TODO: highlight all the elements with memberstack links on the page
+    console.log("highlight all ms links");
+    msHighlightLinks();
+  } else if (message.highlightAttributes) {
+    // TODO: highlight all the elements with memberstack data attributes
+    console.log("highlight all ms attributes");
+    msHighlightAttributes();
+  } else if (message.highlightForms) {
+    console.log("highlight all ms forms");
+    msHighlightForms();
+  }
 }
 chrome.runtime.onMessage.addListener(messageReceived);
--- src/popup.js
+++ src/popup.js
@@ -1,137 +1,137 @@
 window.onload = function () {
-    let tab1 = document.getElementById("tab1");
-    let tab2 = document.getElementById("tab2");
-    let tab3 = document.getElementById("tab3");
+  let tab1 = document.getElementById("tab1");
+  let tab2 = document.getElementById("tab2");
+  let tab3 = document.getElementById("tab3");
 
-    // opens tab inside the popup not a tab in the browser
-    function openTab(evt, tabContentID) {
-        // Declare all variables
-        var i, tabcontent, tablinks;
+  // opens tab inside the popup not a tab in the browser
+  function openTab(evt, tabContentID) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
 
-        // Get all elements with class="tabcontent" and hide them
-        tabcontent = document.getElementsByClassName("tabcontent");
-        for (i = 0; i < tabcontent.length; i++) {
-            tabcontent[i].style.display = "none";
-        }
-
-        // Get all elements with class="tablinks" and remove the class "active"
-        tablinks = document.getElementsByClassName("tablinks");
-        for (i = 0; i < tablinks.length; i++) {
-            tablinks[i].className = tablinks[i].className.replace(" active", "");
-        }
-
-        // Show the current tab, and add an "active" class to the button that opened the tab
-        document.getElementById(tabContentID).style.display = "block";
-        evt.currentTarget.className += " active";
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
     }
 
-    tab1.onclick = function () {
-        openTab(event, 'tabContent1')
-    };
-    tab2.onclick = function () {
-        openTab(event, 'tabContent2')
-    };
-    tab3.onclick = function () {
-        openTab(event, 'tabContent3')
-    };
-    tab1.click();
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
 
-    let msScriptsFoundElmnt = document.getElementById("msScriptsFound");
-    let msScriptVersionElmnt = document.getElementById("msScriptVersion");
-    let msScriptIDElmnt = document.getElementById("msScriptID");
-    let msDataAttributesFoundElmnt = document.getElementById("msDataAttributesFound");
-    let msLinksFoundElmnt = document.getElementById("msLinksFound");
-    let msFormsFoundElmnt = document.getElementById("msFormsFound");
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabContentID).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
 
-    let highlightAttributesElmnt = document.getElementById("highlightAttributes");
-    let highlightLinksElmnt = document.getElementById("highlightLinks");
-    let highlightFormsElmnt = document.getElementById("highlightForms");
-    let scanPageElmnt = document.getElementById("scanPage");
+  tab1.onclick = function () {
+    openTab(event, "tabContent1");
+  };
+  tab2.onclick = function () {
+    openTab(event, "tabContent2");
+  };
+  tab3.onclick = function () {
+    openTab(event, "tabContent3");
+  };
+  tab1.click();
 
-    scanPageElmnt.onclick = msScanPage;
-    highlightAttributesElmnt.onclick = msHighlightAttributes;
-    highlightLinksElmnt.onclick = msHighlightLinks;
-    highlightFormsElmnt.onclick = msHighlightForms;
+  let msScriptsFoundElmnt = document.getElementById("msScriptsFound");
+  let msScriptVersionElmnt = document.getElementById("msScriptVersion");
+  let msScriptIDElmnt = document.getElementById("msScriptID");
+  let msDataAttributesFoundElmnt = document.getElementById(
+    "msDataAttributesFound"
+  );
+  let msLinksFoundElmnt = document.getElementById("msLinksFound");
+  let msFormsFoundElmnt = document.getElementById("msFormsFound");
 
-    let queryParams = {
-        active: true,
-        currentWindow: true
-    };
+  let highlightAttributesElmnt = document.getElementById("highlightAttributes");
+  let highlightLinksElmnt = document.getElementById("highlightLinks");
+  let highlightFormsElmnt = document.getElementById("highlightForms");
+  let scanPageElmnt = document.getElementById("scanPage");
 
-    // messages to send content.js
-    // scanPage
-    // highlightLinks
-    // highlightAttributes
-    // highlightForms
+  scanPageElmnt.onclick = msScanPage;
+  highlightAttributesElmnt.onclick = msHighlightAttributes;
+  highlightLinksElmnt.onclick = msHighlightLinks;
+  highlightFormsElmnt.onclick = msHighlightForms;
 
-    function msScanPage() {
-        function getTabs(tabs) {
-            let message = {
-                scanPage: true
-            };
-            chrome.tabs.sendMessage(tabs[0].id, message);
-        }
-        chrome.tabs.query(queryParams, getTabs);
-    };
+  let queryParams = {
+    active: true,
+    currentWindow: true
+  };
 
-    function msHighlightAttributes() {
-        function getTabs(tabs) {
-            let message = {
-                highlightAttributes: true
-            };
-            chrome.tabs.sendMessage(tabs[0].id, message);
-        }
-        chrome.tabs.query(queryParams, getTabs);
-    };
+  // messages to send content.js
+  // scanPage
+  // highlightLinks
+  // highlightAttributes
+  // highlightForms
 
-    function msHighlightLinks() {
-        function getTabs(tabs) {
-            let message = {
-                highlightLinks: true
-            };
-            chrome.tabs.sendMessage(tabs[0].id, message);
-        }
-        chrome.tabs.query(queryParams, getTabs);
-    };
+  function msScanPage() {
+    function getTabs(tabs) {
+      let message = {
+        scanPage: true
+      };
+      chrome.tabs.sendMessage(tabs[0].id, message);
+    }
+    chrome.tabs.query(queryParams, getTabs);
+  }
 
-    function msHighlightForms() {
-        function getTabs(tabs) {
-            let message = {
-                highlightForms: true
-            };
-            chrome.tabs.sendMessage(tabs[0].id, message);
-        }
-        chrome.tabs.query(queryParams, getTabs);
-    };
+  function msHighlightAttributes() {
+    function getTabs(tabs) {
+      let message = {
+        highlightAttributes: true
+      };
+      chrome.tabs.sendMessage(tabs[0].id, message);
+    }
+    chrome.tabs.query(queryParams, getTabs);
+  }
 
-    function receiver(message, sender, sendResponse) {
-        console.log(message);
-        // message from content.js
-        // msScriptFound
-        // msScripts
-        // msScriptVersions
-        // msIDs
-        // msAttributesFound
-        // msLinksFound
-        // msFormsFound
+  function msHighlightLinks() {
+    function getTabs(tabs) {
+      let message = {
+        highlightLinks: true
+      };
+      chrome.tabs.sendMessage(tabs[0].id, message);
+    }
+    chrome.tabs.query(queryParams, getTabs);
+  }
 
-        if (message.msScripts > 1 ) {
-            msScriptsFoundElmnt.classList.add("--error-text");
-        }
+  function msHighlightForms() {
+    function getTabs(tabs) {
+      let message = {
+        highlightForms: true
+      };
+      chrome.tabs.sendMessage(tabs[0].id, message);
+    }
+    chrome.tabs.query(queryParams, getTabs);
+  }
 
-        if (!message.msScriptFound) {
-            msScriptsFoundElmnt.classList.add("--error-text");
-            msScriptsFoundElmnt.innerHTML = "No MS script found";
-        }
+  function receiver(message, sender, sendResponse) {
+    console.log(message);
+    // message from content.js
+    // msScriptFound
+    // msScripts
+    // msScriptVersions
+    // msIDs
+    // msAttributesFound
+    // msLinksFound
+    // msFormsFound
 
-        msScriptsFoundElmnt.innerHTML = message.msScripts;
-        msScriptVersionElmnt.innerHTML = message.msScriptVersions;
-        msScriptIDElmnt.innerHTML = message.msIDs;
-        msDataAttributesFoundElmnt.innerHTML = message.msAttributesFound;
-        msLinksFoundElmnt.innerHTML = message.msLinksFound;
-        msFormsFoundElmnt.innerHTML = message.msFormsFound;
+    if (message.msScripts > 1) {
+      msScriptsFoundElmnt.classList.add("--error-text");
+    }
 
-    };
-    chrome.runtime.onMessage.addListener(receiver);
+    if (!message.msScriptFound) {
+      msScriptsFoundElmnt.classList.add("--error-text");
+      msScriptsFoundElmnt.innerHTML = "No MS script found";
+    }
 
-}
+    msScriptsFoundElmnt.innerHTML = message.msScripts;
+    msScriptVersionElmnt.innerHTML = message.msScriptVersions;
+    msScriptIDElmnt.innerHTML = message.msIDs;
+    msDataAttributesFoundElmnt.innerHTML = message.msAttributesFound;
+    msLinksFoundElmnt.innerHTML = message.msLinksFound;
+    msFormsFoundElmnt.innerHTML = message.msFormsFound;
+  }
+  chrome.runtime.onMessage.addListener(receiver);
+};
